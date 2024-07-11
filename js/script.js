23f0490ba50c7cdb3f4d33ff4e89aad03cbb42aa
Denise Pingiori <pingiorid@gmail.com>
2024-07-11 16:49:32 +0200

    [upd] added variable and id inner html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //definisco funzione che genera 5 num casuali
-function generateRandomNumbers(){
+function generateRandomNumbers() {
     return Math.floor(Math.random() * 100 + 1);
 }
 
@@ -8,14 +8,24 @@ function createArrayNumbers() {
     let numbers = [];
 
     //verifico se la lunghezza dell'array è < di 5. Se la condizione è vera genero il num e lo pusho nell'array
-    while(numbers.lenght < 5) {
+    while (numbers.length < 5) {
         //genero num con la funzione definita a riga 11
         let num = generateRandomNumbers();
 
         //verifico che il num non sia incluso nell'array
-        if(!numbers.includes(num)) {
+        if (!numbers.includes(num)) {
             //pusho il num nell'array
+            numbers.push(num);
         }
     }
+
     return numbers;
 }
+
+//assegno ad una variabile il val restituito dalla funzione createArrayNumbers
+let random_numbers = createArrayNumbers();
+
+console.log(random_numbers);
+
+//mostro a video, aggangiando id da html
+document.getElementById('numbers').innerHTML = random_numbers
