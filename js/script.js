a8f1a91413a111c3731e4f637bec3f41a2586a13
Denise Pingiori <pingiorid@gmail.com>
2024-07-11 16:34:17 +0200

    [upd] upd array function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 //definisco funzione che genera 5 num casuali
 function generateRandomNumbers(){
     return Math.floor(Math.random() * 100 + 1);
-    
+}
+
+//funzione che genera array numeri
+function createArrayNumbers() {
+    let numbers = [];
+
+    //verifico
 }
