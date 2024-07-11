f39e5ce34ef870a5908d20c2c1548151a7bb6f0f
Denise Pingiori <pingiorid@gmail.com>
2024-07-11 17:07:40 +0200

    [upd] added array num

--- js/script.js
+++ js/script.js
@@ -33,6 +33,24 @@ document.getElementById('numbers').innerHTML = random_numbers;
 setTimeout(function(){
     document.getElementById('numbers').innerHTML = '';
 
+    //creo array vuoto per i num inseriti dall'user
+    let user_numbers = [];
+
+    //ciclo di 5 ripetizioni in cui vado a mostare ad ogni iterazione il prompt di inserimento
+    for (let i = 0; i <5; i++) {
     //permettere all'utente di inserire i suoi num
-    let num = prompt('inserisci il tuo num');
+    user_numbers.push.(prompt('inserisci il tuo num'));
+    }
+
+    //array di num individuati
+    let guessed = [];
+    //controllare che i numeri inseriti dallo user siano presenti nell'array di num casuali
+    for (let i = 0; i <5; i++) {
+        if(random_numbers.includes(user_numbers[i])) {
+            guessed.push(user_numbers[i]);
+        }
+    }
+
+
+
 }, 3000);  
